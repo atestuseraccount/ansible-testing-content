c34d66fae0d2181b16142bd980c33384d6cff06d
James Tanner <tanner.jc@gmail.com>
2018-09-04 11:20:09 -0400

    Add a module util

--- modules/newdeal.py
+++ modules/newdeal.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
 
 import json
+
+from ansible.module_utils.dealio import get_stuff
+
 
 def main():
 
     res = {
         'success': True,
         'changed': True,
-        'lolcats': 50000
+        'lolcats': 50000,
+        'stuff': get_stuff()
     }
     print(json.dumps(res))
 
